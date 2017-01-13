fed850cd1dcd4659303fb7c60e6b6a92f192a525
John Joseph <jmj1444@gmail.com>
2017-01-12 20:25:00 -0500

    Initial commit.

---
+++ dist/app.bundle.js
@@ -0,0 +1,18 @@
+webpackJsonp([0,1],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	console.log('hello world');
+
+/***/ }
+]);
+//# sourceMappingURL=app.bundle.js.map
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const exec = require('child_process').exec;
+
+module.exports = {
+	entry: {
+		app: ['./client/app.js'],
+	},
+	output: {
+		path: "dist",
+		filename: 'app.bundle.js'
+	},
+	module: {
+		preLoaders: [{test: /\.js$/, exclude: /node_modules/, loader: 'jshint-loader'}],
+		loaders: [
+			{test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
+			{test: /\.less$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")},
+			{test: /\.js$/, exclude: /node_modules/, loader: 'babel?presets[]=es2015'},
+			{test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/, loader: "file"}
+		]
+	},
+	devServer: { inline: true },
+	devtool: 'source-map',
+	resolve: {
+		extensions: ['', '.js', '.es6']
+	},
+	plugins : [
+		new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
+		new ExtractTextPlugin("[name].css"),
+		new CopyWebpackPlugin([{ from: 'client/index.html'}])
+	]
+}
